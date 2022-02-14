92f86896db9f3c762761985f3d73d1583115bc51
Keiron O'Shea <keiron.oshea@gmail.com>
2022-02-14 13:02:14 +0000

    Added basic jquery for rendering

--- web/app/static/js/note.js
+++ web/app/static/js/note.js
@@ -21,8 +21,25 @@ function get_note() {
     return json;
 }
 
+function set_heading(dal_id) {
+    console.log(dal_id);
+    document.title = dal_id + " : CLIVSECO";
+    $("#note-heading").html(dal_id);
+}
+
+
+function set_content(note) {
+    console.log(note["discharge_diagnoses"]);
+    $("#presenting-complaint-text").append(note["presenting_complaint"]);
+    $("#clinical-finding-text").append(note["clinical_finding"]);
+    $("#treatment-narrative-text").append(note["treatment_narrative"]);
+    $("#allergy-text").append(note["allergy"]);
+    $("#discharge-diagnoses-text").append(note["discharge_diagnoses"]);
+}
 
 $(document).ready(function () {
     var note = get_note()["content"];
-    console.log(note);
+
+    set_heading(note["dal_id"]);
+    set_content(note);
 });
