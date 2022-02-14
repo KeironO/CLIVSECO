4cb10cf6eb82045a37bc726a8e98d9d38f191d35
Keiron O'Shea <keiron.oshea@gmail.com>
2022-02-14 12:44:05 +0000

    Added JQuery and Basic JavaScript handling

---
+++ web/app/static/js/note.js
@@ -0,0 +1,29 @@
+function get_sample() {
+    var api_url = encodeURI(window.location + '/endpoint');
+
+    var json = (function () {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': api_url,
+            'dataType': "json",
+            'success': function (data) {
+                json = data;
+            },
+            'failure': function (data) {
+                json = data;
+            }
+            
+        });
+        return json;
+    })();
+
+    return json;
+
+}
+
+
+$(document).ready(function () {
+    console.log("JavaScript Injected Correctly :-D");
+});
